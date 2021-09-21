fbf41d2f1343517a3dc152bae97d802ab8232f21
Valber Pereira <valberpereira_@outlook.com>
2021-09-20 23:55:05 -0300

    Primeiro Commit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbusername = 'root';
+    $dbPassword = '';
+    $dbName = 'contato';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName)
+
+    if($conexao->connect_errno)
+    {
+        echo "Erro";
+    }
+    else
+    {
+        echo "Conexão efetuada com sucesso";
+    }
+
+?>
